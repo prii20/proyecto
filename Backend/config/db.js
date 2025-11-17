c8a9e12cec270a1b64dee3fb27e292e3a8d8ffb1
Ruiz Priscila <priscilaruiz217@gmail.com>
2025-11-16 23:02:13 -0300

    Resuelto conflicto después de rebase/merge

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,18 +5,16 @@ dotenv.config();
 
 const connection = mysql.createConnection({
   
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  //  waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+   waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 
-  host:"localhost",
-  user:"root",
-  password:"admin",
-  database:"simulador_lamparas"
+
+
 
 
   
